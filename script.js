4622c169c71de51237aa3b6c11a684e380faa654
MyMac <mymac@MacBook-Pro-4.local>
2020-04-07 00:37:15 -0700

    didn't understand requirements

---
+++ script.js
@@ -0,0 +1,22 @@
+var fortuneWords = ["you will get married at the end of the year", "you will be rich", "you will be able to go outside soon", "you will have many reliable friends", " you will travel next year", "you will be a bridesmaid next year", "you will have a baby or your best friend will have a baby before the year is up", "you will loose money next month", "you will reconnect with an old friend very soon", "you will further your career this week", "stay positive because you will recieve what you sow", "relax because you need it. The future looks stressful", "you will hear from Google Inc sometime soon"]
+var executeButton = document.getElementById("half-button");
+var outputParagraph = document.getElementById("fortune-output");
+var inputElement = document.getElementById('half-input');
+var inputElement2 = document.getElementById('fortune-input');
+var space = " ";
+
+function halfNumber(){
+alert(inputElement/2);
+console.log("half of" + space + inputElement + space + "is" + space + inputElement/2);
+}
+
+
+// halfnumber(document.getElementById("half-input").value);
+
+function fortune () {
+var randomIndex = floor(Math.random() * fortuneWords.length);
+document.randomIndex.innerhtml
+}
+function restyle(){
+
+}
